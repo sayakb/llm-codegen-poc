5dcd54970c699d020f9a52dfdbdff0d8e4958536
Sayak Banerjee <sayakb@gmail.com>
2025-01-02 15:28:04 -0600

    Add Look and Feel component and update routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { LookAndFeelComponent } from './look-and-feel/look-and-feel.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'hello-world', component: HelloWorldComponent }
+  { path: 'hello-world', component: HelloWorldComponent },
+  { path: 'look-and-feel', component: LookAndFeelComponent },
 ];
---
+++ src/app/look-and-feel/look-and-feel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-look-and-feel',
+  imports: [],
+  templateUrl: './look-and-feel.component.html',
+  styleUrl: './look-and-feel.component.css'
+})
+export class LookAndFeelComponent {
+
+}
